68e527904ce84781bbc58122676584f4af77689e
Alex D. Zharichenko <azharichenko@gmail.com>
2020-05-12 08:23:52 -0400

    Adding in calendar api from pittapi

---
+++ progression/scaper.py
@@ -0,0 +1,20 @@
+import re
+import progression.cal as pitt_cal
+from datetime import datetime
+from pprint import pprint
+
+FINALS_START = re.compile("FINAL EXAM(|S|INATION)\s[\w\s]+?UNDERGRAD(|S)\s")
+SPRING_START = "SPRING TERM CLASSES BEGIN"
+FALL_START = "FALL TERM CLASSES BEGIN"
+
+def find_next_semester():
+    pass
+
+for event in pitt_cal.get_academic_calendar():
+    if 'All Campus Dates' in event.meta:
+        if FALL_START in event.title.upper():
+            print(event)
+            print(datetime.strptime(event.date, '%Y-%m-%d').date())
+        if SPRING_START in event.title.upper():
+            print(event)
+            print(datetime.strptime(event.date, '%Y-%m-%d').date())
